ac5194879b0dec379e95ecea4a70ea4ca1b8207c
Dmgonzo97 <109998281+Dmgonzo97@users.noreply.github.com>
2023-01-27 17:36:28 -0500

    finalization of heroku deployment

--- app.py
+++ app.py
@@ -3,13 +3,14 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow 
 from flask_cors import CORS 
 from flask_bcrypt import Bcrypt
-import os
+# import os
 
 app = Flask(__name__)
 
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
+# basedir = os.path.abspath(os.path.dirname(__file__))
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://bpqjwuibujsjsv:ebe943c91f8fe980a0bfb7b8fe03ca0a85f38d67e9242e4719ee08a9118bbcb8@ec2-52-201-124-168.compute-1.amazonaws.com:5432/d9l4vjjahr66n'
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 bcrypt = Bcrypt(app)
