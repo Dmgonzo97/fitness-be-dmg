b5ab5b67775332e601c86a04a1c86a0768bb7673
Dmgonzo97 <109998281+Dmgonzo97@users.noreply.github.com>
2023-03-08 21:42:57 -0500

    forgotten to add get_jwt import, line 12

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from flask_cors import CORS
 from flask_bcrypt import Bcrypt
 
 from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt
 from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import jwt_required
 from flask_jwt_extended import JWTManager
